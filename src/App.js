9375a11cb4c10744bc5857203e39f7b1f4f6ba10
Felicia <felicia.wang.08@gmail.com>
2020-08-07 16:23:50 -0500

    Completed initial asynchronous fetchData function within api folder's index.js. Plans to populate wanted data into Cards component.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css'
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+	state = {
+		data: {}, // empty until populated
+	}
+
+	async componentDidMount() {
+		const fetchedData = await fetchData();
+		
+		this.setState({ data: fetchedData });
+	}
+
 	render() {
 		return (
 			<div className = {styles.container}>
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'; // handles APIs
 
 const url = 'https://covid19.mathdro.id/api'
+
+export const fetchData = async () => { //Asynchronous function
+	try {
+		// take only data that we need
+		const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url); 
+
+		const modifiedData = { confirmed, recovered, deaths, lastUpdate };
+		return modifiedData; 
+	} catch (error) {
+	
+	}
+}
