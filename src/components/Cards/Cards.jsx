5fe4680aecd74209fe07c415f49a8064d9b76066
Felicia <felicia.wang.08@gmail.com>
2020-08-08 16:24:15 -0500

    Updated cards to make them look more visually pleasing.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,47 +3,102 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
 import cx from 'classnames';
+import { makeStyles } from '@material-ui/core/styles';
+import BrandCardHeader from '@mui-treasury/components/cardHeader/brand';
+import TextInfoContent from '@mui-treasury/components/content/textInfo';
+import { useN03TextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/n03';
+import { useLightTopShadowStyles } from '@mui-treasury/styles/shadow/lightTop';
+
+//Bottom useGridStyles and useStyles graciously taken and modified from MUI Treasury
+const useGridStyles = makeStyles(({ breakpoints }) => ({
+	root: {
+		[breakpoints.up('md')]: {
+			justifyContent: 'center',
+		},
+	},
+}));
+
+const useStyles = makeStyles(() => ({
+	root: {
+		maxWidth: 343,
+		borderRadius: 20,
+		margin: 20,
+	},
+	content: {
+		padding: 24,
+	},
+}));
+
 
 // Creating grid that will space out and center Cards
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+	const gridStyles = useGridStyles();
+	const styles = useN03TextInfoContentStyles();
+	const shadowStyles = useLightTopShadowStyles();
+	const cardStyles = useStyles();
+
 	console.log(confirmed);
 	if (!confirmed) { // If confirmed data not yet ready, return 'Loading'
 		return 'Loading...';
 	}
 	return (
 		<div className = {styles.container} >
-			<Grid container spacing = {3} justify="center"> 
-				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-					<CardContent>
-						<Typography color="textSecondary" gutterBottom>Confirmed Cases</Typography>
-						<Typography variant="h5">
-							<CountUp start={0} end={confirmed.value} duration={2.5} separator=","
-							/>
-						</Typography> 
-						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-						<Typography variant="body2">Number of confirmed active cases</Typography>
+			<Grid classes={gridStyles} container spacing = {4} wrap={'nowrap'}> 
+				<Grid item component={Card} xs={12} md={3} className={cx(cardStyles.root, shadowStyles.root)}>
+					<BrandCardHeader
+						image={'https://img.favpng.com/1/12/3/computer-icons-png-favpng-7SbJJ8GfuwRk8C3kFBUqYbbte.jpg'}	
+						extra={'LAST UPDATED: ', new Date(lastUpdate).toDateString()}
+					/>
+					<CardContent className={cardStyles.content}>
+						<TextInfoContent
+							classes={styles}
+							overline={'CONFIRMED CASES'}
+							heading={
+								<CountUp start={0} end={confirmed.value} duration={2.5} separator=","
+								/>
+							}
+							body={
+								'Number of confirmed active cases of COVID-19.'
+							}
+						/>
 					</CardContent>
 				</Grid>
-				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-					<CardContent>
-						<Typography color="textSecondary" gutterBottom>Recovered</Typography>
-						<Typography variant="h5">
-							<CountUp start={0} end={recovered.value} duration={2.5} separator=","
-							/>
-						</Typography> 
-						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-						<Typography variant="body2">Number of confirmed recoveries</Typography>
+				<Grid item component={Card} xs={12} md={3} className={cx(cardStyles.root, shadowStyles.root)}>
+					<BrandCardHeader
+						image={'https://www.vippng.com/png/detail/452-4525287_health-benefits-clipart-health-icon-green-png.png'}	
+						extra={new Date(lastUpdate).toDateString()}
+					/>
+					<CardContent className={cardStyles.content}>
+						<TextInfoContent
+							classes={styles}
+							overline={'RECOVERIES'}
+							heading={
+								<CountUp start={0} end={recovered.value} duration={2.5} separator=","
+								/>
+							}
+							body={
+								'Number of confirmed confirmed recoveries from COVID-19.'
+							}
+						/>
 					</CardContent>
 				</Grid>
-				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-					<CardContent>
-						<Typography color="textSecondary" gutterBottom>Deaths</Typography>
-						<Typography variant="h5">
-							<CountUp start={0} end={deaths.value} duration={2.5} separator=","
-							/>
-						</Typography> 
-						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-						<Typography variant="body2">Number of confirmed deaths</Typography>
+				<Grid item component={Card} xs={12} md={3} className={cx(cardStyles.root, shadowStyles.root)}>
+					<BrandCardHeader
+						image={'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2/128/death1-circle-red-512.png'}	
+						extra={new Date(lastUpdate).toDateString()}
+					/>
+					<CardContent className={cardStyles.content}>
+						<TextInfoContent
+							classes={styles}
+							overline={'DEATHS'}
+							heading={
+								<CountUp start={0} end={deaths.value} duration={2.5} separator=","
+								/>
+							}
+							body={
+								'Number of confirmed confirmed deaths from COVID-19.'
+							}
+						/>
 					</CardContent>
 				</Grid>
 			</Grid>
