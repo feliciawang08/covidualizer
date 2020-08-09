ffb04053f8957f67915ff23be07f5290cbba9a01
Felicia <felicia.wang.08@gmail.com>
2020-08-09 01:37:54 -0500

    Added CDC link at bottom as well as GitHub and LinkedIn profiles; also added copyright

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css'
 import { fetchData } from './api';
 import titleImage from './images/covidualizer.png';
+import Emoji from 'react-emoji-render';
 
 class App extends React.Component {
 
@@ -33,8 +33,22 @@ class App extends React.Component {
 				<Cards data={data} />
 				<CountryPicker handleCountryChange={this.handleCountryChange} />
 				<Chart data = { data } country = { country }  /> 
+				<div>
+					<p>
+						Learn how to <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html" target="_blank">stop the spread.</a> 
+					</p>
+				</div> 
+				<div>
+					<p>
+						<a href="https://github.com/feliciawang08" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/felicia-wang-08/" target="_blank"> LinkedIn</a>
+					</p>
+				</div>
+				<div className={styles.bottom}>
+					<Emoji text="Made with :heart: by Felicia Wang | Est. Aug. 2020" /> 
 			</div>
-		)}
+			</div>
+
+	)}
 		
 	}
 
