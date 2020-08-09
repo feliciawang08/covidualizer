91caaf095ad2e4dd58ed455ba90e59bc3bd162a9
Felicia <felicia.wang.08@gmail.com>
2020-08-08 22:57:06 -0500

    Updates

---
+++ precache-manifest.109cc5dcab6bb0acc7f7c09bf8c9c785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b4d041faed2fa3f254f78cab9a9a0d",
+    "url": "/covidualizer/index.html"
+  },
+  {
+    "revision": "2cf96c8dfa2677636e4a",
+    "url": "/covidualizer/static/css/main.b8808ef7.chunk.css"
+  },
+  {
+    "revision": "8e7278127afab13728df",
+    "url": "/covidualizer/static/js/2.7ca4517b.chunk.js"
+  },
+  {
+    "revision": "bbe2dbbdaea8525fcc46934c5c58f863",
+    "url": "/covidualizer/static/js/2.7ca4517b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf96c8dfa2677636e4a",
+    "url": "/covidualizer/static/js/main.38e7f6bb.chunk.js"
+  },
+  {
+    "revision": "c3ce8c5730a5fb8c26c1",
+    "url": "/covidualizer/static/js/runtime-main.738aa32c.js"
+  },
+  {
+    "revision": "5006a434993eaf23cd4ff734ffc07ae4",
+    "url": "/covidualizer/static/media/covidualizer.5006a434.png"
+  }
+]);
